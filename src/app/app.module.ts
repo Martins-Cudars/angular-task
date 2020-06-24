fd45c0b1f140af176afa5205dd1628e74ab00e5c
Martins Cudars <martins.cudars@gmail.com>
2020-06-24 18:11:45 +0300

    add header, footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './global/site-header/site-header.component';
+import { SiteFooterComponent } from './global/site-footer/site-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent
   ],
   imports: [
     BrowserModule,
